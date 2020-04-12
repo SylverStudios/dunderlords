3bb961b5f0d80a267d24dcbe6ea4ae601b790dea
Aaron Votre <avotre422@gmail.com>
2020-04-12 19:35:02 -0400

    🚛 Move view code to it's own file

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,7 @@
-module Main exposing (init, main, update, view)
+module Main exposing (init, main, update)
 
 import Browser
 import Hero exposing (Alliance(..))
-import Html exposing (..)
-import Html.Attributes exposing (..)
 import Model exposing (Model, Msg(..))
 import View
 
@@ -34,40 +32,6 @@ update message model =
 
 
 -- ---------------------------
--- VIEW
--- ---------------------------
-
-
-view : Model -> Html Msg
-view model =
-    div [ class "container" ]
-        [ header [] [ h1 [] [ text "Dunderlords Alliance Builder" ] ]
-        , div [ class "body" ] [ View.team model.team ]
-        , allianceSummary model
-        ]
-
-
-allianceSummary : Model -> Html Msg
-allianceSummary { team } =
-    let
-        allianceCount =
-            Hero.summary team
-    in
-    div [ class "alliance-summary" ]
-        [ h2 [] [ text "Alliances" ]
-        , div [] [ img [ class "alliance", alt "Brawny Alliance Badge", src "/images/alliances/brawny.png" ] [], text <| "Brawny: " ++ String.fromInt allianceCount.brawny ]
-        , div [] [ img [ class "alliance", alt "Heartless Alliance Badge", src "/images/alliances/heartless.png" ] [], text <| "Heartless: " ++ String.fromInt allianceCount.heartless ]
-        , div [] [ img [ class "alliance", alt "Primordial Alliance Badge", src "/images/alliances/primordial.png" ] [], text <| "Primordial: " ++ String.fromInt allianceCount.primordial ]
-        , div [] [ img [ class "alliance", alt "Savage Alliance Badge", src "/images/alliances/savage.png" ] [], text <| "Savage: " ++ String.fromInt allianceCount.savage ]
-        , div [] [ img [ class "alliance", alt "Scaled Alliance Badge", src "/images/alliances/scaled.png" ] [], text <| "Scaled: " ++ String.fromInt allianceCount.scaled ]
-        , div [] [ img [ class "alliance", alt "Spirit Alliance Badge", src "/images/alliances/spirit.png" ] [], text <| "Spirit: " ++ String.fromInt allianceCount.spirit ]
-        , div [] [ img [ class "alliance", alt "Troll Alliance Badge", src "/images/alliances/troll.png" ] [], text <| "Troll: " ++ String.fromInt allianceCount.troll ]
-        , div [] [ img [ class "alliance", alt "Warrior Alliance Badge", src "/images/alliances/warrior.png" ] [], text <| "Warrior: " ++ String.fromInt allianceCount.warrior ]
-        ]
-
-
-
--- ---------------------------
 -- MAIN
 -- ---------------------------
 
@@ -80,7 +44,7 @@ main =
         , view =
             \m ->
                 { title = "Elm 0.19 starter"
-                , body = [ view m ]
+                , body = [ View.view m ]
                 }
         , subscriptions = \_ -> Sub.none
         }
--- src/View.elm
+++ src/View.elm
@@ -1,9 +1,18 @@
-module View exposing (team)
+module View exposing (view)
 
 import Hero exposing (Alliance(..), Hero)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Model exposing (Msg)
+import Model exposing (Model, Msg)
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "container" ]
+        [ header [] [ h1 [] [ text "Dunderlords Alliance Builder" ] ]
+        , div [ class "body" ] [ team model.team ]
+        , allianceSummary model.team
+        ]
 
 
 team : List Hero -> Html Msg
@@ -16,4 +25,23 @@ heroMini { name } =
     div []
         [ img [ class "hero", alt "Slardar Mini Badge", src "/images/minis/slardar.png" ] []
         , text name
+        ]
+
+
+allianceSummary : List Hero -> Html Msg
+allianceSummary heroes =
+    let
+        allianceCount =
+            Hero.summary heroes
+    in
+    div [ class "alliance-summary" ]
+        [ h2 [] [ text "Alliances" ]
+        , div [] [ img [ class "alliance", alt "Brawny Alliance Badge", src "/images/alliances/brawny.png" ] [], text <| "Brawny: " ++ String.fromInt allianceCount.brawny ]
+        , div [] [ img [ class "alliance", alt "Heartless Alliance Badge", src "/images/alliances/heartless.png" ] [], text <| "Heartless: " ++ String.fromInt allianceCount.heartless ]
+        , div [] [ img [ class "alliance", alt "Primordial Alliance Badge", src "/images/alliances/primordial.png" ] [], text <| "Primordial: " ++ String.fromInt allianceCount.primordial ]
+        , div [] [ img [ class "alliance", alt "Savage Alliance Badge", src "/images/alliances/savage.png" ] [], text <| "Savage: " ++ String.fromInt allianceCount.savage ]
+        , div [] [ img [ class "alliance", alt "Scaled Alliance Badge", src "/images/alliances/scaled.png" ] [], text <| "Scaled: " ++ String.fromInt allianceCount.scaled ]
+        , div [] [ img [ class "alliance", alt "Spirit Alliance Badge", src "/images/alliances/spirit.png" ] [], text <| "Spirit: " ++ String.fromInt allianceCount.spirit ]
+        , div [] [ img [ class "alliance", alt "Troll Alliance Badge", src "/images/alliances/troll.png" ] [], text <| "Troll: " ++ String.fromInt allianceCount.troll ]
+        , div [] [ img [ class "alliance", alt "Warrior Alliance Badge", src "/images/alliances/warrior.png" ] [], text <| "Warrior: " ++ String.fromInt allianceCount.warrior ]
         ]
