17a0439658912962e845be183b27c138a161a211
Aaron Votre <avotre422@gmail.com>
2020-04-18 10:57:21 -0400

    🚚 Move Hero and Alliance to Model dir

---
+++ src/Model/Alliance.elm
@@ -0,0 +1,45 @@
+module Model.Alliance exposing (Alliance(..), imagePath)
+
+
+type Alliance
+    = Brawny
+    | Heartless
+    | Primordial
+    | Savage
+    | Scaled
+    | Spirit
+    | Troll
+    | Warrior
+
+
+imagePath : Alliance -> String
+imagePath name =
+    "/images/alliances/" ++ toString name ++ ".png"
+
+
+toString : Alliance -> String
+toString name =
+    case name of
+        Brawny ->
+            "brawny"
+
+        Heartless ->
+            "heartless"
+
+        Primordial ->
+            "primordial"
+
+        Savage ->
+            "savage"
+
+        Scaled ->
+            "scaled"
+
+        Spirit ->
+            "spirit"
+
+        Troll ->
+            "troll"
+
+        Warrior ->
+            "warrior"
