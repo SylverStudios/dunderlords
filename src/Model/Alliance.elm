4a36dbf40950aab8e81681bbac7ebd695d7b3822
Sam Roberts <samgqroberts@gmail.com>
2020-04-19 15:40:50 -0400

    add remaining Alliances to Alliance.elm

--- src/Model/Alliance.elm
+++ src/Model/Alliance.elm
@@ -2,13 +2,31 @@ module Model.Alliance exposing (Alliance(..), imagePath, toString)
 
 
 type Alliance
-    = Brawny
+    = Assassin
+    | Bloodbound
+    | Brawny
+    | Brute
+    | Champion
+    | Deadeye
+    | Demon
+    | Dragon
+    | Druid
+    | Healer
     | Heartless
+    | Human
+    | Hunter
+    | Insect
+    | Knight
+    | Mage
     | Primordial
     | Savage
     | Scaled
     | Spirit
+    | Summoner
     | Troll
+    | Vigilant
+    | Void
+    | Warlock
     | Warrior
 
 
@@ -20,11 +38,53 @@ imagePath name =
 toString : Alliance -> String
 toString name =
     case name of
+        Assassin ->
+            "assassin"
+
+        Bloodbound ->
+            "bloodbound"
+
         Brawny ->
             "brawny"
 
+        Brute ->
+            "brute"
+
+        Champion ->
+            "champion"
+
+        Deadeye ->
+            "deadeye"
+
+        Demon ->
+            "demon"
+
+        Dragon ->
+            "dragon"
+
+        Druid ->
+            "druid"
+
+        Healer ->
+            "healer"
+
         Heartless ->
             "heartless"
+
+        Human ->
+            "human"
+
+        Hunter ->
+            "hunter"
+
+        Insect ->
+            "insect"
+
+        Knight ->
+            "knight"
+
+        Mage ->
+            "mage"
 
         Primordial ->
             "primordial"
@@ -38,8 +98,20 @@ toString name =
         Spirit ->
             "spirit"
 
+        Summoner ->
+            "summoner"
+
         Troll ->
             "troll"
+
+        Vigilant ->
+            "vigilant"
+
+        Void ->
+            "void"
+
+        Warlock ->
+            "warlock"
 
         Warrior ->
             "warrior"
