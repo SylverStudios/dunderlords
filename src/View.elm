d2f3bf9b23eabb4ceb253ac6f17baa09cbd757cd
Aaron Votre <avotre422@gmail.com>
2020-04-19 12:05:50 -0400

    🎨 Update layout, group Crew + Alliance under Team

--- src/View.elm
+++ src/View.elm
@@ -1,7 +1,7 @@
 module View exposing (view)
 
 import Dict.Any
-import Html exposing (Html, button, div, h1, h2, header, img, section, span, text)
+import Html exposing (Html, button, div, h1, h2, h3, header, img, section, text)
 import Html.Attributes exposing (class, src)
 import Html.Events exposing (onClick)
 import Model exposing (Model, Msg(..))
@@ -13,41 +13,17 @@ import View.Icon
 
 view : Model -> Html Msg
 view model =
-    let
-        refreshButton : Html Msg
-        refreshButton =
-            button [ class "rounded", onClick Refresh ]
-                [ img [ src "/images/refresh-24px.svg" ] [] ]
-    in
     div [ class "container" ]
         [ header [] [ h1 [] [ text "Dunderlords Alliance Builder" ] ]
         , section []
-            [ header [] [ h2 [] [ text "Team" ], refreshButton ]
-            , team model.team
+            [ header [] [ h2 [] [ text "Team" ] ]
+            , teamSection model
             ]
         , section []
             [ h2 [] [ text "Hero Pool" ]
             , heroPool
             ]
-        , section []
-            [ h2 [] [ text "Alliances" ]
-            , allianceSummary model.team
-            ]
         ]
-
-
-team : List Hero -> Html Msg
-team heroes =
-    let
-        lineupOrEmtpyText =
-            case heroes of
-                [] ->
-                    [ span [ class "empty-team" ] [ text "Click a Hero below to begin building a team" ] ]
-
-                list ->
-                    List.map (heroMini Remove) list
-    in
-    div [ class "team" ] lineupOrEmtpyText
 
 
 heroPool : Html Msg
@@ -69,8 +45,39 @@ heroMini msg hero =
     hero |> View.Icon.icon |> View.Icon.withMsg msg |> View.Icon.toHtml
 
 
-allianceSummary : List Hero -> Html Msg
-allianceSummary heroes =
+teamSection : Model -> Html Msg
+teamSection { team } =
+    case team of
+        [] ->
+            div [ class "crew-empty" ] [ text "Click a Hero below to begin building a team" ]
+
+        atLeastOne :: remaining ->
+            div [ class "crew-summary" ]
+                [ crew atLeastOne remaining
+                , alliances (atLeastOne :: remaining)
+                ]
+
+
+{-| Display Hero icons
+They remove on click
+pass one first so we guarentee that we have a crew available
+-}
+crew : Hero -> List Hero -> Html Msg
+crew firstHero remaining =
+    let
+        refreshButton : Html Msg
+        refreshButton =
+            button [ class "rounded", onClick Refresh ]
+                [ img [ src "/images/refresh-24px.svg" ] [] ]
+    in
+    div [ class "crew" ]
+        [ div [ class "header" ] [ h3 [] [ text "Crew" ], refreshButton ]
+        , div [ class "crew-icons" ] <| List.map (heroMini Remove) (firstHero :: remaining)
+        ]
+
+
+alliances : List Hero -> Html Msg
+alliances heroes =
     let
         activeAlliances =
             heroes
@@ -80,7 +87,10 @@ allianceSummary heroes =
                 |> List.reverse
                 |> List.map (\( alliance, count ) -> allianceCounter alliance count)
     in
-    div [ class "alliance-summary" ] activeAlliances
+    div [ class "alliances" ]
+        [ div [ class "header" ] [ h3 [] [ text "Alliances" ] ]
+        , div [ class "alliance-icons" ] activeAlliances
+        ]
 
 
 allianceCounter : Alliance -> Int -> Html msg
